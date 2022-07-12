07a64daa475971616f6e3dc438766ea6e0a90e98
LineWelder <redalerm@gmail.com>
2022-07-12 11:57:23 +0200

    Start implementing pattern parsing

--- src/lib.rs
+++ src/lib.rs
@@ -20,3 +20,27 @@ struct State {
 pub struct Pattern {
     states: Vec<State>,
 }
+
+#[derive(PartialEq, Eq)]
+#[cfg_attr(test, derive(Debug))]
+pub enum PatternParsingError {
+    EmptyInput,
+}
+
+impl TryFrom<&str> for Pattern {
+    type Error = PatternParsingError;
+
+    fn try_from(input: &str) -> Result<Self, PatternParsingError> {
+        Err(PatternParsingError::EmptyInput)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{Matching::*, PatternParsingError::*, *};
+
+    #[test]
+    fn empty_regex_is_error() {
+        assert_eq!(Pattern::try_from(""), Err(EmptyInput));
+    }
+}
